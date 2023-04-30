a75902998d8d3ffc694081fc2a8cfe99e682f80d
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-04-30 17:05:11 -0300

    Create cards object
    src: url for the image
    click: how many times it was clicked
    id: uniqid

---
+++ src/game-logic/cardsObj.js
@@ -0,0 +1,28 @@
+import uniqid from 'uniqid';
+
+export const cards = [
+  {src: './src/assets/batman.png', click: 0, id: uniqid()},
+  {src: './src/assets/flash.png', click: 0, id: uniqid()},
+  {src: './src/assets/lex-luthor.png', click: 0, id: uniqid()},
+  {src: './src/assets/c3po.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/green-lantern.png', click: 0, id: uniqid()},
+  {src: './src/assets/magneto.png', click: 0, id: uniqid()},
+  {src: './src/assets/captain-america.png', click: 0, id: uniqid()},
+  {src: './src/assets/harleyquinn.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/reverse-flash.png', click: 0, id: uniqid()},
+  {src: './src/assets/catwoman.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/hulk.png', click: 0, id: uniqid()},
+  {src: './src/assets/spider-man.png', click: 0, id: uniqid()},
+  {src: './src/assets/chewbacca.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/indiana-jones.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/superman.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/clone-trooper.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/iron-man.png', click: 0, id: uniqid()},
+  {src: './src/assets/wolverine.png', click: 0, id: uniqid()},
+  {src: './src/assets/darth-vader.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/jar-jar-binks.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/wonder-woman.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/doctor-doom.png', click: 0, id: uniqid()},
+  {src: './src/assets/joker.jpg', click: 0, id: uniqid()},
+  {src: './src/assets/yoda.jpg', click: 0, id: uniqid()},
+]
